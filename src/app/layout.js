21deb469a7b5a7ca0656c3b04d48711c8f721d13
shubham-shootorder <shubham@shootorder.in>
2024-10-12 10:27:53 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         {/* Bootstrap 5.3.2 CDN */}
-        <link rel="icon" href="/icon.ico" />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
