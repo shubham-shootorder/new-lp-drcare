d98bc86a03669be6199bca5f29274555b6865fda
shubham-shootorder <shubham@shootorder.in>
2024-10-16 12:47:21 +0530

    changes in the number and the content number

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -18,13 +18,13 @@ const Header = () => {
               </a>
               <div className="d-flex">
                 <div className="d-flex">
-                  <a href="tel:+91 8019690003" className="wbtn d-md-block d-none">
-                    +91 8019690003
+                  <a href="tel:+91 7997993400" className="wbtn d-md-block d-none">
+                    +91 7997993400
                   </a>
                   &nbsp; &nbsp;
                   {/* WhatsApp icon - Visible on mobile only */}
                   <a
-                    href={`https://api.whatsapp.com/send/?phone=8019690003&text&app_absent=0`}
+                    href={`https://api.whatsapp.com/send/?phone=7997993400&text&app_absent=0`}
                     className="whatsupchat d-block d-md-none"
                     target="_blank"
                     rel="noopener noreferrer"
